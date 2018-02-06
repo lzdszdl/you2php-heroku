9738eaba2992c9fa27a34b59e3568f82d3de7b6f
lzdszdl <136208872@qq.com>
2018-02-06 17:37:47 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', 'AIzaSyCDXtojFs2zglr4VCPLR59QD-4yj-KCtFI');
+define('GJ_CODE', 'FR');
+define('SITE_NAME', '皮皮虾油图笔');
+define('TITLENAME', '皮皮虾');
+define('EN2DEKEY', 'VTyZJVNScgRJej03LDRLJ9kv3greEfn7');
+define('EMAIL', 'admin@alei.ac.cn');
 ?>
